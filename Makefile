82a9b394889f71e9d28bb03def84990fd64e8fb7
Andy Aylward <aaylward@gmail.com>
2014-07-21 13:51:17 -0400

    better make thanks @ldriscoll

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 SOURCE_DIR = src
 TEST_DIR = test
+CFLAGS = -std=c99 -Wall -Werror -pedantic
 SOURCES = section_2_8.c
 DEPS = $(TEST_DIR)/test_runner.c $(addprefix $(SOURCE_DIR)/, $(SOURCES))
 OBJS = test_runner.o section_2_8.o
 
 all:
 	rm -f run_tests
-	gcc -c $(DEPS)
+	gcc -c $(CFLAGS) $(DEPS)
 	gcc -o run_tests $(OBJS)
 	rm -f *.o
 
--- test/test_util.h
+++ test/test_util.h
@@ -7,12 +7,12 @@
 #define SUCCESS 0
 
 /* TEST functions that return `int` */
-#define ASSERT_INT( MESSAGE , TEST , EXPECTED ) ({ \
+#define ASSERT_INT( MESSAGE , EXPECTED , TEST ) { \
     int _actual = TEST; \
     if (_actual != EXPECTED) { \
         printf("%s Expected: %d, Got: %d\n", MESSAGE, EXPECTED, _actual); \
         return ERROR; \
     } \
-})
+}
 
 #endif
